8d276754e2129908b2345c58ee678e0725bc5223
Fahmii19 <fahmitb70@gmail.com>
2025-01-18 00:01:29 +0700

    Update Galeri component with new images and additional gallery items for enhanced visual content

--- src/pages/Galeri.svelte
+++ src/pages/Galeri.svelte
@@ -21,7 +21,7 @@
             <img
               alt="gallery"
               class="absolute inset-0 w-full h-full object-cover object-center"
-              src="https://dummyimage.com/600x360"
+              src="./assets/galeri/galeri1.jpg"
             />
             <div
               class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100"
@@ -46,7 +46,7 @@
             <img
               alt="gallery"
               class="absolute inset-0 w-full h-full object-cover object-center"
-              src="https://dummyimage.com/601x361"
+              src="./assets/galeri/galeri2.jpg"
             />
             <div
               class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100"
@@ -71,7 +71,7 @@
             <img
               alt="gallery"
               class="absolute inset-0 w-full h-full object-cover object-center"
-              src="https://dummyimage.com/603x363"
+              src="./assets/galeri/galeri3.jpg"
             />
             <div
               class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100"
@@ -96,7 +96,7 @@
             <img
               alt="gallery"
               class="absolute inset-0 w-full h-full object-cover object-center"
-              src="https://dummyimage.com/602x362"
+              src="./assets/galeri/galeri4.jpg"
             />
             <div
               class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100"
@@ -121,7 +121,7 @@
             <img
               alt="gallery"
               class="absolute inset-0 w-full h-full object-cover object-center"
-              src="https://dummyimage.com/605x365"
+              src="./assets/galeri/galeri5.jpg"
             />
             <div
               class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100"
@@ -146,7 +146,82 @@
             <img
               alt="gallery"
               class="absolute inset-0 w-full h-full object-cover object-center"
-              src="https://dummyimage.com/606x366"
+              src="./assets/galeri/galeri6.jpg"
+            />
+            <div
+              class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100"
+            >
+              <h2
+                class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1"
+              >
+                THE SUBTITLE
+              </h2>
+              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                Alper Kamu
+              </h1>
+              <p class="leading-relaxed">
+                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
+                microdosing tousled waistcoat.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="lg:w-1/3 sm:w-1/2 p-4">
+          <div class="flex relative">
+            <img
+              alt="gallery"
+              class="absolute inset-0 w-full h-full object-cover object-center"
+              src="./assets/galeri/galeri7.jpg"
+            />
+            <div
+              class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100"
+            >
+              <h2
+                class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1"
+              >
+                THE SUBTITLE
+              </h2>
+              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                Alper Kamu
+              </h1>
+              <p class="leading-relaxed">
+                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
+                microdosing tousled waistcoat.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="lg:w-1/3 sm:w-1/2 p-4">
+          <div class="flex relative">
+            <img
+              alt="gallery"
+              class="absolute inset-0 w-full h-full object-cover object-center"
+              src="./assets/galeri/galeri8.jpg"
+            />
+            <div
+              class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100"
+            >
+              <h2
+                class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1"
+              >
+                THE SUBTITLE
+              </h2>
+              <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                Alper Kamu
+              </h1>
+              <p class="leading-relaxed">
+                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
+                microdosing tousled waistcoat.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="lg:w-1/3 sm:w-1/2 p-4">
+          <div class="flex relative">
+            <img
+              alt="gallery"
+              class="absolute inset-0 w-full h-full object-cover object-center"
+              src="./assets/galeri/galeri9.jpg"
             />
             <div
               class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100"

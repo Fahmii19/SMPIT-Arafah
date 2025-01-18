41f638146d73f526dc0820fdecef6492829d73a8
Fahmii19 <fahmitb70@gmail.com>
2025-01-18 08:03:31 +0700

    Add lazy loading to images in TenagaPendidik component for improved performance

--- src/pages/TenagaPendidik.svelte
+++ src/pages/TenagaPendidik.svelte
@@ -12,6 +12,7 @@
             src="https://static.wixstatic.com/media/811433_3e589b5d350c4c839afa42e54cbdded3~mv2.png"
             alt="Team Member 1"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Dra. H.Kokom Komariah</h3>
           <p class="text-gray-700">Ketua Yayasan Bina Insancita Depok</p>
@@ -23,6 +24,7 @@
             src="https://static.wixstatic.com/media/811433_8fd6bb9876a74d5998c564e56c233338~mv2.png"
             alt="Team Member 2"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Drs. H.Pepen Supena, M.M</h3>
           <p class="text-gray-700">Direktur Pendidikan SIT ARAFAH</p>
@@ -34,6 +36,7 @@
             src="https://static.wixstatic.com/media/811433_8264076fc25d43f39d1866f9b8959dca~mv2.png"
             alt="Team Member 3"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Lidiya Dewi Kencana, S.Pd</h3>
           <p class="text-gray-700">Kepala SMP IT ARAFAH</p>
@@ -45,6 +48,7 @@
             src="https://static.wixstatic.com/media/811433_eeda1c574431486882a660bd03350ab4~mv2.png"
             alt="Team Member 4"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Nurul Istiqomah, S.Pd</h3>
           <p class="text-gray-700">Bendahara BOS</p>
@@ -56,6 +60,7 @@
             src="https://static.wixstatic.com/media/811433_c9f9b83f80b64b6484db59d2b73335cc~mv2.png"
             alt="Team Member 5"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Anida Nur R, S.Mat</h3>
           <p class="text-gray-700">Wakabid Kurikulum Mapel Matematika</p>
@@ -67,6 +72,7 @@
             src="https://static.wixstatic.com/media/811433_408ec15da3904f48bfe0d44c6c016d58~mv2.png"
             alt="Team Member 6"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Dede Hidayat</h3>
           <p class="text-gray-700">
@@ -80,6 +86,7 @@
             src="https://static.wixstatic.com/media/811433_28fc4aeb17754d6bb86897ed753ec880~mv2.png"
             alt="Team Member 7"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Ayu Diah Aryani S, S.SI</h3>
           <p class="text-gray-700">Wakabid Humas & SIM Mapel Informatika</p>
@@ -91,6 +98,7 @@
             src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
             alt="Team Member 8"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Mita Retnolarasaty, S.Pd</h3>
           <p class="text-gray-700">Pembina OSIS Mapel Bahasa Inggris dan EA</p>
@@ -102,6 +110,7 @@
             src="https://static.wixstatic.com/media/811433_9968019e3b1a43468bce9b3abd3ffb9c~mv2.png"
             alt="Team Member 1"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Andre Septiyanta</h3>
           <p class="text-gray-700">Koordinator TTA Mapel Bahasa Arab dan TTA</p>
@@ -113,6 +122,7 @@
             src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
             alt="Team Member 2"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Sarlita Hidayati, S.Pd</h3>
           <p class="text-gray-700">
@@ -126,6 +136,7 @@
             src="https://static.wixstatic.com/media/811433_f1c8b343bfb04999a768e379523cd3ce~mv2.png"
             alt="Team Member 3"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Andi Ardiansyah, S.Pd.I</h3>
           <p class="text-gray-700">Koordinator K7 Mapel Agama Islam dan TTA</p>
@@ -137,6 +148,7 @@
             src="https://static.wixstatic.com/media/811433_c4e88a834bad41f98c20dd351d44febb~mv2.png"
             alt="Team Member 4"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Ayu Setiowati, S.S</h3>
           <p class="text-gray-700">
@@ -150,6 +162,7 @@
             src="https://static.wixstatic.com/media/811433_412f8266ed974c7bb33394cc74d667a4~mv2.png"
             alt="Team Member 5"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Muhamad Nuraeni, S.Pd</h3>
           <p class="text-gray-700">Koordinator Sarpras Mapel PJOK</p>
@@ -161,6 +174,7 @@
             src="https://static.wixstatic.com/media/811433_67081e41f4ae4b048a9367f0305f808f~mv2.png"
             alt="Team Member 6"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Melisa Afriyanti, S.Kom</h3>
           <p class="text-gray-700">Tata Usaha ( Keuangan )</p>
@@ -172,6 +186,7 @@
             src="https://static.wixstatic.com/media/811433_6e33026a3abb468f9da67c23f161e1f9~mv2.png"
             alt="Team Member 7"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Nuraini, S.Pd</h3>
           <p class="text-gray-700">Tata Usaha ( Administrasi )</p>
@@ -183,6 +198,7 @@
             src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
             alt="Team Member 8"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Siti Fadhilah, S.Pd.I</h3>
           <p class="text-gray-700">Operator Dapodik</p>
@@ -195,6 +211,7 @@
             src="https://static.wixstatic.com/media/811433_1ed3a64171ee41e9b26bee42adf0ea83~mv2.png"
             alt="Team Member 1"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Retno Kusumaningtyas, S.Pd</h3>
           <p class="text-gray-700">Mapel IPA</p>
@@ -206,6 +223,7 @@
             src="https://static.wixstatic.com/media/811433_161039c21afe4e2186b292b0efdf62ee~mv2.png"
             alt="Team Member 2"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Nais Ranny, S.Pd</h3>
           <p class="text-gray-700">Mapel Bahasa Indonesia dan PKN</p>
@@ -217,6 +235,7 @@
             src="https://static.wixstatic.com/media/811433_00eb4fbbe16f45e9871a5a660d3409f7~mv2.png"
             alt="Team Member 3"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Siti Haifah, M.Pd</h3>
           <p class="text-gray-700">Mapel Bahasa Indonesia dan SBK</p>
@@ -228,6 +247,7 @@
             src="https://static.wixstatic.com/media/811433_07e7c188adbd42359f64629da38ce4b0~mv2.png"
             alt="Team Member 4"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Rofiatul Husnia, S.Pd.I</h3>
           <p class="text-gray-700">Tahsin Tahfidz Al-Quran</p>
@@ -239,6 +259,7 @@
             src="https://static.wixstatic.com/media/811433_cda1fe6548d24ea8b9b0b9f3c81137c2~mv2.png"
             alt="Team Member 5"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Nia Kurniasari, S.Pd</h3>
           <p class="text-gray-700">Mapel IPA dan Bahasa Sunda</p>
@@ -250,6 +271,7 @@
             src="https://static.wixstatic.com/media/811433_b1688309bdc94f20b56d63403f0cf223~mv2.png"
             alt="Team Member 6"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Neti Nurbaiti, S.Pd</h3>
           <p class="text-gray-700">Bimbingan Konseling dan UKS</p>
@@ -261,6 +283,7 @@
             src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
             alt="Team Member 7"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Atika Munifatu Z, S.Pd</h3>
           <p class="text-gray-700">Mapel IPS dan PKN ​</p>
@@ -272,6 +295,7 @@
             src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
             alt="Team Member 8"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Mutia Dila K, S.Pd</h3>
           <p class="text-gray-700">Mapel IPS dan TTA</p>
@@ -283,6 +307,7 @@
             src="https://static.wixstatic.com/media/811433_3fb33b7c8fbd42b3b8519f31ad91f381~mv2.png"
             alt="Team Member 1"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Hikni Solihin, S.Pd.I</h3>
           <p class="text-gray-700">Mapel Bahasa Sunda dan TTA</p>
@@ -294,6 +319,7 @@
             src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
             alt="Team Member 2"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Rafi Mu'allim Fathoni</h3>
           <p class="text-gray-700">Mapel Bahasa Arab dan TTA</p>
@@ -305,6 +331,7 @@
             src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
             alt="Team Member 3"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Muhammad Rasyiq M</h3>
           <p class="text-gray-700">English Area dan TTA</p>
@@ -316,6 +343,7 @@
             src="https://static.wixstatic.com/media/811433_c85f3b61776f491aac9a75c28d17dfd0~mv2.png"
             alt="Team Member 4"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Ahmad Daung</h3>
           <p class="text-gray-700">Keamanan</p>
@@ -327,6 +355,7 @@
             src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
             alt="Team Member 3"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Misan</h3>
           <p class="text-gray-700">Keamanan</p>
@@ -338,6 +367,7 @@
             src="https://static.wixstatic.com/media/811433_f02ef691935e4507846c669789d025c3~mv2.png"
             alt="Team Member 5"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Azis Suseno</h3>
           <p class="text-gray-700">Tata Laksana</p>
@@ -349,6 +379,7 @@
             src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
             alt="Team Member 3"
             class="w-full rounded-full mb-4"
+            loading="lazy"
           />
           <h3 class="text-xl font-semibold mb-2">Wandi</h3>
           <p class="text-gray-700">Tata Laksana</p>
